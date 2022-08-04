10125d74b93997fa9bfaffb66e3c7bd3e2fd10a4
Natalia Vera <nvera@coopebombas.com>
2022-08-04 09:10:46 -0500

    Initial Commit

---
+++ AppWordle/AppWordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWordleApp.swift
+//  AppWordle
+//
+//  Created by Desarrollador1 on 4/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AppWordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppWordle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppWordle
+//
+//  Created by Desarrollador1 on 4/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
